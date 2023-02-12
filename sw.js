590c0b5734bcd09633f90855597ada5340ffce28
Pulkit Pareek <84959590+pulkitpareek18@users.noreply.github.com>
2023-02-12 09:56:44 +0530

    corrected some code

---
+++ sw.js
@@ -0,0 +1,24 @@
+// Adding this example caching code with no Url's to cache just to tell the browser that this app will work offline so that it can prommpt PWA install.
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+
+ ];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
