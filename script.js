57cb355e41c955904ec28245153768fad7588aa3
Pulkit Pareek <84959590+pulkitpareek18@users.noreply.github.com>
2023-07-30 12:53:35 +0530

    refactor: 2Embed to Stream Embed api

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ function scrollToTop() {
   }
 }
 
+const htmlBody = document.querySelector("html, body");
+const results = document.querySelector("#results");
+
+const scrollToResults = () => {
+  const resultsTop = results.offsetTop;
+  htmlBody.scrollTo({
+    top: resultsTop,
+    behavior: "smooth",
+  });
+};
+
+
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 const resultsContainer = document.getElementById("results");
@@ -39,7 +51,7 @@ function fetchAndShow() {
           let imageAndInfo = "";
 
           if (result.qid === "movie" && result.i) {
-            imageAndInfo = `<a onClick="setUrl(this); return setVideo(this);" url="imdb=${result.id}&type=movie&title=${result.l.replace(/ /g, "_")}" isWebSeries="false" title="${result.l}"  class="links" IMDB="${result.id}" href="https://www.2embed.cc/embed/${result.id}" target="_blank">
+            imageAndInfo = `<a onClick="setUrl(this); return setVideo(this);" url="imdb=${result.id}&type=movie&title=${result.l.replace(/ /g, "_")}" isWebSeries="false" title="${result.l}"  class="links" IMDB="${result.id}" href="https://multiembed.mov/directstream.php?video_id=${result.id}" target="_blank">
                      <img alt="${result.l}" src="${optimisedImageUrl(result.i.imageUrl)}">
                       <div class="info">
                        <h3>${result.l}</h3>
@@ -47,7 +59,7 @@ function fetchAndShow() {
                       </div>
                    </a>`;
           } else if (result.qid === "tvSeries" && result.i) {
-            imageAndInfo = `<a onClick="setUrl(this); return setVideo(this);" url="imdb=${result.id}&season=1&episode=1&title=${result.l.replace(/ /g, "_")}" IMDB="${result.id}" title="${result.l}" isWebSeries="true" class="links" href="https://www.2embed.cc/embedtv/${result.id}&s=1&e=1" target="_blank">
+            imageAndInfo = `<a onClick="setUrl(this); return setVideo(this);" url="imdb=${result.id}&season=1&episode=1&title=${result.l.replace(/ /g, "_")}" IMDB="${result.id}" title="${result.l}" isWebSeries="true" class="links" href="https://multiembed.mov/directstream.php?video_id=${result.id}&s=1&e=1" target="_blank">
                       <img alt="${result.l}" src="${optimisedImageUrl(result.i.imageUrl)}">
                         <div class="info">
                           <h3>${result.l}</h3>
@@ -76,7 +88,7 @@ function setAll(imdb, title, season, episode, type) {
     a.setAttribute("title", title);
     a.setAttribute("class", "links");
     a.setAttribute("IMDB", imdb);
-    a.setAttribute("href", "https://www.2embed.cc/embed/" + imdb);
+    a.setAttribute("href", " https://multiembed.mov/directstream.php?video_id=" + imdb);
     a.setAttribute("target", "_blank");
     a.click();
   } else if (imdb && title && episode && !type) {
@@ -91,7 +103,7 @@ function setAll(imdb, title, season, episode, type) {
     a.setAttribute("IMDB", imdb);
     a.setAttribute(
       "href",
-      `https://www.2embed.cc/embedtv/${imdb}&s=${season}&e=${episode}`
+      `https://multiembed.mov/directstream.php?video_id=${imdb}&s=${season}&e=${episode}`
     );
     a.setAttribute("target", "_blank");
     a.click();
@@ -210,7 +222,7 @@ window.onpopstate = function () {
   }
 };
 
-//  ajax auto search by input which will execute when user stops typing for 500ms
+// auto search by input which will execute when user stops typing for 500ms
 
 let timer;
 
@@ -221,12 +233,7 @@ searchInput.addEventListener("keyup", function () {
     updateURL(inputQuery);
     fetchAndShow();
     window.dispatchEvent(new PopStateEvent("popstate"));
-    $("html,body").animate(
-      {
-        scrollTop: $("#results").offset().top,
-      },
-      "slow"
-    );
+    scrollToResults();
   }, 500); // wait for 500ms before executing the function
 });
 
@@ -234,6 +241,7 @@ searchInput.addEventListener("keyup", function () {
 
 searchButton.addEventListener("click", function () {
   fetchAndShow();
+  scrollToResults();
   // hide information
   window.dispatchEvent(new PopStateEvent("popstate"));
 });
@@ -329,7 +337,7 @@ function setVideo(element) {
             minimumIntegerDigits: 2,
             useGrouping: false,
           });
-          episodesData += `<a class="episodes" title="${seasonsDataJSON.name + ": E" + formatedEpisodeNumber + ". " + episode.name}" cssidentification="s${seasonNumber}e${episodeNumber}" url="imdb=${imdbID}&season=${seasonNumber}&episode=${episodeNumber}&title=${seasonsDataJSON.name.replace(/ /g, "_") + "_E" + formatedEpisodeNumber + "_" + episode.name.replace(/ /g, "_")}" onClick="event.preventDefault();setVideo(this);setUrl(this); " href="https://www.2embed.cc/embedtv/${showId}&s=${seasonNumber}&e=${episodeNumber}">E${formatedEpisodeNumber}. ${episode.name}</a>`;
+          episodesData += `<a class="episodes" title="${seasonsDataJSON.name + ": E" + formatedEpisodeNumber + ". " + episode.name}" cssidentification="s${seasonNumber}e${episodeNumber}" url="imdb=${imdbID}&season=${seasonNumber}&episode=${episodeNumber}&title=${seasonsDataJSON.name.replace(/ /g, "_") + "_E" + formatedEpisodeNumber + "_" + episode.name.replace(/ /g, "_")}" onClick="event.preventDefault();setVideo(this);setUrl(this); " href="https://multiembed.mov/directstream.php?video_id=${imdbID}&s=${seasonNumber}&e=${episodeNumber}">E${formatedEpisodeNumber}. ${episode.name}</a>`;
         }
 
         episodeContainer.innerHTML = episodesData;
